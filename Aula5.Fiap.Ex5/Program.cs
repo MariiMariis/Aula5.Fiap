d58642318b66fc1ebe15f47cbfb6b3822ac79a7d
MariiMariis <mariana.motta94@gmail.com>
2022-05-31 21:57:37 -0300

    Exercicio 6

---
+++ Aula5.Fiap.Ex5/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+
+namespace Aula5.Fiap.Ex5
+{
+    //Enum meses do ano, com valores pré determinados
+    enum MesesAno
+    {
+        Janeiro = 1,
+        Fevereiro,
+        Marco,
+        Abril,
+        Maio,
+        Junho,
+        Julho,
+        Agosto,
+        Setembro,
+        Outubro,
+        Novembro,
+        Dezembro
+
+    };
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("O {0}º mês do ano é {1}",(int)MesesAno.Abril, MesesAno.Abril );
+
+        }
+    }
+}
---
+++ Aula5.Fiap.Ex6/Teste_enum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula5.Fiap.Ex5
+{
+    class Teste_enum
+    {
+        // Lista com itens pré determinados
+        public enum Atletas
+        {
+            Daiane, Robinho, Ronaldo, Messi, Marta
+        };
+
+        static void Main(string[] args)
+        {
+            //Criação de contador para o loop
+            int contador = 0;
+
+            // Passará por cada item na lista do enumerate, e realizara uma ação contando cada ítem 
+            foreach (Atletas atletas in Enum.GetValues(typeof(Atletas)))
+            {
+                contador++;
+                // O método Write imprime o resultado na mesma linha
+                Console.Write(contador + "º Lugar"+ "----");
+                // o método writeline imprime um resultado e pula uma linha para imprimir o próximo
+                Console.WriteLine(atletas);
+            }
+        }
+    }
+}
